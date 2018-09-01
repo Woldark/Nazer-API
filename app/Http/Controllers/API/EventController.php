73f5d718d33202e4806970a388ac9a9be6b659c8
hatamiarash7 <hatamiarash7@gmail.com>
2018-09-01 15:27:47 +0430

    add application's id

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Application;
 use App\Event;
 use App\Http\Controllers\Controller;
 use App\Libraries\JDF;
@@ -14,12 +15,16 @@ class EventController extends Controller
     {
         $body = $request->get('body');
         $key = $request->get('key');
+        $app = $request->get('app');
+
         $user = (new User)->where("key", $key)->first();
+        $application = (new Application)->where("uid", $app)->first();
 
         $jdf = new JDF();
 
         $event = new Event();
         $event->user_id = $user->id;
+        $event->application_id = $application->id;
         $event->body = $body;
         $event->create_date = $jdf->getTimestamp();
         $event->save();
